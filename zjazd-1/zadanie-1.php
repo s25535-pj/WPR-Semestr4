7fab06765acf0186d183183584da1386d15a2270
s25535-pj <s25535@pjwstk.edu.pl>
2023-03-12 16:15:33 +0100

    Add zjazd-1, add zadanie-1.php

---
+++ zjazd-1/zadanie-1.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html >
+<html>
+
+<head>
+<meta charset="utf-8">
+<meta lang="pl">
+<title>zadanie-1</title>
+<!-- <link href="zadanie1.css" rel="stylesheet" type="text/css"> -->
+</head>
+<body>
+
+<?php
+function throwTheDice(){
+	echo "The dice returned number:"." ".rand(1,6);
+}
+throwTheDice();
+?>
+
+</body>
+</html>
